da4ab0aa38a7de89cc56244ee84dd371579c7e18
jeffmhopkins <jeff.m.hopkins@gmail.com>
2020-04-10 08:57:04 -0400

    Updating to #define based CC

--- src/owp_synth/owp_synth.ino
+++ src/owp_synth/owp_synth.ino
@@ -6,12 +6,66 @@
  * Please use the included TouchOSC GUI for ipad for full control. Lots of CC. 
  */
 
+#define CC_MODULATION_WHEEL               1
+#define CC_BREATH                         2
+#define CC_EXPRESSION                     11
+#define CC_BREATH_TO_PULSE_WIDTH          20
+#define CC_BREATH_TO_FILTER_CUTOFF        21
+#define CC_BREATH_TO_FILTER_RESONANCE     22
+#define CC_BREATH_TO_OVERDRIVE            23
+#define CC_MODULATION_TO_PULSE_WIDTH      24
+#define CC_MODULATION_TO_FILTER_CUTOFF    25
+#define CC_MODULATION_TO_FILTER_RESONANCE 26
+#define CC_MODULATION_TO_OVERDRIVE        27
+#define CC_EXPRESSION_TO_PULSE_WIDTH      28
+#define CC_EXPRESSION_TO_FILTER_CUTOFF    29
+#define CC_EXPRESSION_TO_FILTER_RESONANCE 30
+#define CC_EXPRESSION_TO_PORTAMENTO       31
+#define CC_EXPRESSION_TO_OVERDRIVE        32
+#define CC_PORTAMENTO_MIN                 33
+#define CC_WAV_PLAYER                     34
+#define CC_WAV_PLAYER_GAIN                35
+#define CC_LOAD_PATCH                     36
+#define CC_SAVE_PATCH                     37
+#define CC_WAVE1_SHAPE                    41
+#define CC_WAVE2_SHAPE                    42
+#define CC_WAVE3_SHAPE                    43
+#define CC_WAVE4_SHAPE                    44
+#define CC_WAVE1_GAIN                     51
+#define CC_WAVE2_GAIN                     52
+#define CC_WAVE3_GAIN                     53
+#define CC_WAVE4_GAIN                     54
+#define CC_WAVE1_DETUNE_MULTIPLIER        61
+#define CC_WAVE2_DETUNE_MULTIPLIER        62
+#define CC_WAVE3_DETUNE_MULTIPLIER        63
+#define CC_WAVE4_DETUNE_MULTIPLIER        64
+#define CC_WAVE1_PULSE_WIDTH              71
+#define CC_WAVE2_PULSE_WIDTH              72
+#define CC_WAVE3_PULSE_WIDTH              73
+#define CC_WAVE4_PULSE_WIDTH              74
+#define CC_FILTER_RESONANCE_OFFSET        76
+#define CC_NOTE_OFFSET                    77
+#define CC_FINE_TUNING                    78
+#define CC_MASTER_VOLUME                  81
+#define CC_FILTER_FREQUENCY_OFFSET        82
+#define CC_NOISE_PINK_GAIN                83
+#define CC_NOISE_WHITE_GAIN               84
+#define CC_EFFECTS_MIXER_CLEAN            85
+#define CC_EFFECTS_MIXER_FLANGE           86
+#define CC_EFFECTS_MIXER_CHORUS           87
+#define CC_OSCILLATOR_OVERDRIVE           89
+#define CC_EFFECTS_MIXER_DELAY            90
+#define CC_REVERB_ROOM_SIZE               91
+#define CC_EFFECTS_MIXER_DELAY_MS         92
+
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
 #include <limits.h>
+
+extern float driven_waveform[257];
 
 // GUItool: begin automatically generated code
 AudioSynthWaveform       wave4;          //xy=139,219
@@ -90,11 +144,9 @@ AudioConnection          patchCord40(left, 0, audio_output, 0);
 AudioControlSGTL5000     sgtl5000_1;     //xy=1551,65
 // GUItool: end automatically generated code
 
-extern float driven_waveform[257];
-
 //Global Variables - Settings (You can change these to set startup conditions)
-int   pitchbend_range                = 2;    //0
-int   note_offset                    = 16;   //1
+int   pitchbend_range                = 2;    
+int   note_offset                    = 16;   
 float default_tune                   = 1.0;  //2
 bool  pitchbend_reset_on_noteon      = true; //3
 int   portamento_min                 = 50;   //4
@@ -141,7 +193,7 @@ int   breath_gamma                   = 1.85; //40
 float delay_amount                   = 250;  //41
 float master_volume                  = 0.8;
 float effects_oscillators            = 1.0;
-float effects_flanger                = 0.0;
+float effects_flange                 = 0.0;
 float effects_chorus                 = 0.0;
 float effects_delay                  = 0.0;
 float effects_delay_ms               = 0.0;
@@ -185,20 +237,14 @@ int n_chorus = 4;
 
 
 void setup() {
-
   Serial.begin(115200);
-  delay(5000);
-
   configureSD();
-  
   sgtl5000_1.enable();
   sgtl5000_1.volume(master_volume);
   AudioMemory(1500);
   sgtl5000_1.audioPostProcessorEnable();
 
   AudioNoInterrupts();
-
- 
   wave1.begin(0.0, 440, WAVEFORM_TRIANGLE_VARIABLE);
   wave1.pulseWidth(wave1_width.read());
   wave2.begin(0.0, 440, WAVEFORM_TRIANGLE_VARIABLE);
@@ -231,10 +277,7 @@ void setup() {
   mixer1.gain(2, noise_white_gain);//white noise
   mixer1.gain(3, distortion_gain);//waveshape1
       waveshape1.shape(driven_waveform, 257);
- 
-  
   effect_mixer.gain(0,1);//normal
-
   effect_mixer.gain(1,0);//flange
       flange1.begin(delayline,FLANGE_DELAY_LENGTH,s_idx,s_depth,s_freq);
   effect_mixer.gain(2,0);//chorus
@@ -248,15 +291,12 @@ void setup() {
       delay1.delay(1, delay_amount*2);
       delay1.delay(2, delay_amount*3);
       delay1.delay(3, delay_amount*4);
- 
   glide_frequency.amplitude(0.0);
   filter_frequency.amplitude(0.0);
-
   breath.amplitude(0.0);
   expression.amplitude(0.0);
   pitchbend.amplitude(0.0);
   modulation.amplitude(0.0);
-
   AudioInterrupts();
 }
 
@@ -290,305 +330,29 @@ void loop() {
   waveselect.gain(3, wave4_gain);
   noise_pink.amplitude(breath.read());
   noise_white.amplitude(breath.read());
-
-  
   filter_frequency.amplitude(bound1(breath_to_filter_cutoff * breath.read() + 
                              modulation_to_filter_cutoff * modulation.read() +
-                             expression_to_filter_cutoff * expression.read()), filter_frequency_ramp_rate);
-                             
+                             expression_to_filter_cutoff * expression.read()), filter_frequency_ramp_rate);               
   filter.setLowpass(filter_passes, 
                     filter_max_frequency * filter_frequency.read() + filter_min_frequency, 
                     bound1(filter_q + breath_to_filter_resonance * breath.read() +
                                       modulation_to_filter_resonance * modulation.read() +
-                                      expression_to_filter_resonance * expression.read()));
-                                      
+                                      expression_to_filter_resonance * expression.read()));                    
   mixer1.gain(0, 1.0-overdrive_calculated);
   mixer1.gain(1, noise_pink_gain);
   mixer1.gain(2, noise_white_gain);                             
   mixer1.gain(3, overdrive_calculated);
-  
   effect_mixer.gain(0, effects_oscillators);
-  effect_mixer.gain(1, effects_flanger);
+  effect_mixer.gain(1, effects_flange);
   effect_mixer.gain(2, effects_chorus);
   effect_mixer.gain(3, effects_delay);
-
+  left.gain(2, wav_gain);
+  right.gain(2, wav_gain);
   sgtl5000_1.volume(master_volume);
   reverb.roomsize(reverb_size);
-  
   AudioInterrupts();
-  
   if (usbMIDI.read()) {
     processMIDI();
-  }
-}
-
-void processMIDI(void) {
-  byte type, channel, data1, data2, cable;
-  type = usbMIDI.getType();       // which MIDI message, 128-255
-  channel = usbMIDI.getChannel(); // which MIDI channel, 1-16
-  data1 = usbMIDI.getData1();     // first data byte of message, 0-127
-  data2 = usbMIDI.getData2();     // second data byte of message, 0-127
-  cable = usbMIDI.getCable();     // which virtual cable with MIDIx8, 0-7
-
-  switch (type) {
-    case usbMIDI.NoteOff: // 0x80
-      if(note == data1) { //if this noteOff message matches are currently playing note
-        breath.amplitude(0.0, breath_ramp_rate_note_off);
-        noteon = false;
-      }
-      break;
-
-    case usbMIDI.NoteOn: // 0x90
-      if(noteon) {
-        if(pitchbend_reset_on_noteon) {
-          pitchbend.amplitude(0.0);
-        }
-        glide_frequency.amplitude((pow(2.0, ((float)(data1+note_offset)/12.0))/wave_freq), portamento_min + expression.read() * (portamento_max - portamento_min) * expression_to_portamento);
-      } else {
-        glide_frequency.amplitude((pow(2.0, ((float)(data1+note_offset)/12.0))/wave_freq));
-      }
-      note = data1;
-      noteon = true;
-      break;
-
-    case usbMIDI.ControlChange: // 0xB0
-      if(data1==1) {//Modulation Wheel
-        modulation.amplitude(data2/127.0, modulation_ramp_rate);
-      }
-      if(data1==2) {//Breath
-        if(noteon) {
-          breath.amplitude(lin_to_log(data2, 127, breath_gamma)/127.0, breath_ramp_rate);
-        }
-      }
-      if(data1==11) {//Expression
-        expression.amplitude(data2/127.0, expression_ramp_rate);
-      }
-      if(data1==20) {
-        breath_to_pulse_width = data2/127.0;
-      }
-      if(data1==21) {
-        breath_to_filter_cutoff = data2/127.0;
-      }
-      if(data1==22) {
-        breath_to_filter_resonance = data2/127.0;
-      }
-      if(data1==23) {
-        breath_to_overdrive = data2/127.0;
-      }
-      if(data1==24) {
-        modulation_to_pulse_width = data2/127.0;
-      }
-      if(data1==25) {
-        modulation_to_filter_cutoff = data2/127.0;
-      }
-      if(data1==26) {
-        modulation_to_filter_resonance = data2/127.0;
-      }
-      if(data1==27) {
-        modulation_to_overdrive = data2/127.0;
-      }
-      if(data1==28) {
-        expression_to_pulse_width = data2/127.0;
-      }
-      if(data1==29) {
-        expression_to_filter_cutoff = data2/127.0;
-      }
-      if(data1==30) {
-        expression_to_filter_resonance = data2/127.0;
-      }
-      if(data1==31) {
-        expression_to_portamento = data2/127.0;
-      }
-      if(data1==32) {
-        expression_to_overdrive = data2/127.0;
-      }
-      if(data1==33) {
-        portamento_min = data2/127.0*250;
-      }
-      if(data1==34) {
-        if(data2==0) {
-          wavplayer.stop();
-        } else {
-          if(data2/127.0)
-          if(wavplayer.isPlaying()) {
-              wavplayer.stop();
-          }
-          wavplayer.play(String(String(data2, DEC) +".WAV").c_str());
-        }
-         
-      }
-      if(data1==35) {
-        wav_gain = data2/127.0;
-        left.gain(2, wav_gain);
-        right.gain(2, wav_gain);
-      }
-      if(data1==36) {
-        AudioNoInterrupts();
-        loadPatch(data2);
-        AudioInterrupts();
-      }
-      if(data1==37) {
-        AudioNoInterrupts();
-        savePatch(data2);
-        AudioInterrupts();
-      }
-      if(data1==41) {//Oscillator Shapes (41-44)
-        if(data2 < 32) {
-          wave1.begin(WAVEFORM_SINE);
-          wave1_shape = WAVEFORM_SINE;
-        } else if(data2 < 64) {
-          wave1.begin(WAVEFORM_SQUARE);
-          wave1_shape = WAVEFORM_SQUARE;
-        } else if(data2 < 96) {
-          wave1.begin(WAVEFORM_SAWTOOTH);
-          wave1_shape = WAVEFORM_SAWTOOTH;
-        } else {
-          wave1.begin(WAVEFORM_TRIANGLE_VARIABLE);
-          wave1_shape = WAVEFORM_TRIANGLE_VARIABLE;
-        }
-      }
-      if(data1==42) {
-        if(data2 < 32) {
-          wave2.begin(WAVEFORM_SINE);
-        } else if(data2 < 64) {
-          wave2.begin(WAVEFORM_SQUARE);
-        } else if(data2 < 96) {
-          wave2.begin(WAVEFORM_SAWTOOTH);
-        } else {
-          wave2.begin(WAVEFORM_TRIANGLE_VARIABLE);
-        }
-      }
-      if(data1==43) {
-        if(data2 < 32) {
-          wave3.begin(WAVEFORM_SINE);
-        } else if(data2 < 64) {
-          wave3.begin(WAVEFORM_SQUARE);
-        } else if(data2 < 96) {
-          wave3.begin(WAVEFORM_SAWTOOTH);
-        } else {
-          wave3.begin(WAVEFORM_TRIANGLE_VARIABLE);
-        }
-      }
-      if(data1==44) {
-        if(data2 < 32) {
-          wave4.begin(WAVEFORM_SINE);
-        } else if(data2 < 64) {
-          wave4.begin(WAVEFORM_SQUARE);
-        } else if(data2 < 96) {
-          wave4.begin(WAVEFORM_SAWTOOTH);
-        } else {
-          wave4.begin(WAVEFORM_TRIANGLE_VARIABLE);
-        }
-      }
-      if(data1==51) {//Oscillator Gains (51-54)
-        
-        wave1_gain = (data2/127.0);
-      }
-      if(data1==52) {
-        wave2_gain = (data2/127.0);
-      }
-      if(data1==53) {
-        wave3_gain = (data2/127.0);
-      }
-      if(data1==54) {
-        wave4_gain = (data2/127.0);
-      }
-      if(data1==61) {//Oscillator Detune (61-64)
-        wave1_detune_multiplier = data2/127.0*0.75+0.25;
-      }
-      if(data1==62) {
-        wave2_detune_multiplier = data2/127.0*0.75+0.25;
-      }
-      if(data1==63) {
-        wave3_detune_multiplier = data2/127.0*0.75+0.25;
-      }
-      if(data1==64) {
-        wave4_detune_multiplier = data2/127.0*0.75+0.25;
-      }
-      if(data1==70) {//Breath to Pulse Width
-        breath_to_pulse_width = data2/127.0;
-      }
-      if(data1==71) {//Oscilator Pulse Widths (71-74)
-        wave1_width.amplitude(data2/127.0);
-        wave1_pulse_width = (data2/127.0);
-      }
-      if(data1==72) {
-        wave2_width.amplitude(data2/127.0);
-        wave2_pulse_width = (data2/127.0);
-      }
-      if(data1==73) {
-        wave3_width.amplitude(data2/127.0);
-        wave3_pulse_width = (data2/127.0);
-      }
-      if(data1==74) {
-        wave4_width.amplitude(data2/127.0);
-        wave4_pulse_width = (data2/127.0);
-      }
-      if(data1==75) {//Modulation to Pulse Width
-        modulation_to_pulse_width = data2/127.0;
-      }
-      if(data1==76) {//Filter Resonance
-        filter_q = data2/127.0*3;
-      }
-      if(data1==77) {//Note Offset
-        note_offset=data2/127.0 * 24;
-      }
-      if(data1==78) {//Fine Tuning
-        default_tune=(data2/127.0/2+0.75);
-      }
-      if(data1==81) {//vol
-        master_volume = data2/127.0;
-      }
-      if(data1==82) {//FilterOffset
-        filter_min_frequency = data2/127.0*5000.0;
-      }
-      if(data1==83) {//Pink Noise Mix
-        noise_pink_gain = data2/127.0;
-      }
-      if(data1==84) {//White Noise Mix
-        noise_white_gain = data2/127.0;
-      }
-      if(data1==85) {//Clean
-        effects_oscillators = data2/127.0;
-      }
-      if(data1==86) {//flange mix
-        effects_flanger = data2/127.0;
-      }
-      if(data1==87) {//chorus mix
-         effects_chorus = data2/127.0;
-      }
-      if(data1==89) {//Distortion from waveshape1
-        distortion_mix = data2/127.0;
-        
-      }
-      if(data1==90) {//Delay mix
-        effects_delay = data2/127.0;
-      }
-      
-      if(data1==91) {//Reverb Roomsize
-        reverb_size = data2/127.0;
-        
-      }
-      if(data1==92){//delay milliseconds
-        //Not setting this in the loo
-        AudioNoInterrupts();
-        effects_delay_ms = data2/127.0 * 250;
-        delay1.delay(0, effects_delay_ms);
-        delay1.delay(1, effects_delay_ms*2);
-        delay1.delay(2, effects_delay_ms*3);
-        delay1.delay(3, effects_delay_ms*4);
-        AudioInterrupts();
-      }
-      
-      break;
-
-
-    case usbMIDI.PitchBend: // 0xE0
-      pitchbend.amplitude((data1+data2*128.0-8192.0)/8192.0, pitchbend_ramp_rate);
-      break;
-
-    default:
-      break;
   }
 }
 
